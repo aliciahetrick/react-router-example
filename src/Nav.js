d2bba71711634a7af51f7d0e2b929663756c4509
aliciahetrick <aliciahetrick@gmail.com>
2023-02-05 13:44:42 -0500

    Add links to the routes

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Nav() {
+  const navStyle = {
+    color: 'white',
+  }
+
   return (
     <>
       <nav className="nav">
-        <h3>Logo</h3>
+        <Link to="/" style={navStyle}>
+          <h3>Logo</h3>
+        </Link>
         <ul className="nav-links">
-          <li>About</li>
-          <li>Shop</li>
+          <Link to="/about" style={navStyle}>
+            <li>About</li>
+          </Link>
+          <Link to="shop" style={navStyle}>
+            <li>Shop</li>
+          </Link>
         </ul>
       </nav>
     </>
