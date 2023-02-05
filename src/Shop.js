fc6d6256f492462f69afb57c5440329911123652
aliciahetrick <aliciahetrick@gmail.com>
2023-02-05 14:14:39 -0500

    Add pokemon api data and create dynamic links

--- src/Shop.js
+++ src/Shop.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Shop() {
+  useEffect(() => {
+    fetchItems()
+  }, [])
+
+  const [pokemon, setItems] = useState([])
+
+  const fetchItems = async () => {
+    const data = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20')
+    const pokemonData = await data.json()
+    // console.log(pokemon.results)
+    console.log(pokemonData)
+    setItems(pokemonData.results)
+  }
+
   return (
     <>
-      <h1>Shop Page</h1>
+      {pokemon.map((monster) => {
+        return (
+          <h1 key={monster.name}>
+            <Link to={`shop/${monster.name}`}>{monster.name}</Link>
+          </h1>
+        )
+      })}
     </>
   )
 }
